090ded60fcc7b69ffbb3ee95b92fd9f2657a84ff
Tori Jones <Tori@Trystian-Amedee-Joness-MacBook-Pro.local>
2018-10-25 18:44:56 -0500

    done

---
+++ 8_1.js
@@ -0,0 +1,11 @@
+for(var i = 1; i<=100; i++){
+    if(i%2 == 1){
+        continue;
+        console.log("IsPrime " + True);
+    
+    }
+}
+    else{
+        console.log("IsPrime " + False);
+
+    }
